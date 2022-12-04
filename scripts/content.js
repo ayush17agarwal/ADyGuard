2dfeceb2770c02964f37b13f053d350a54cc5558
Ayush Agarwal <ayushba2@illinois.edu>
2022-12-04 17:14:28 -0600

    fixed request bugs

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,43 @@
+function isAvailableEngine(engine) {
+    // generated from https://docs.webscrapingapi.com/google-search-api/google-search-engines
+    const available_engines = [
+        "google",
+        "google_maps",
+        "google_maps_reviews",
+        "google_autocomplete",
+        "google_scholar",
+        "google_scholar_profiles",
+        "google_scholar_author",
+        "google_scholar_cite",
+        "google_product",
+        "google_reverse_image",
+        "google_jobs",
+        "google_jobs_listing",
+        "google_events",
+        "google_play",
+        "google_trends"
+    ];
+
+    return available_engines.includes(engine);
+};
+
+function create_endpoint(engine, API_KEY) {
+    var endpoint = "https://serpapi.webscrapingapi.com/v1";
+    if (isAvailableEngine(engine)) {
+        endpoint += "?api_key=" + API_KEY + "&engine=" + engine;
+        return endpoint;
+    } else {
+        throw EndpointException("Invalid endpoint. Please contact admin");
+    }
+
+};
+
+
+var API_KEY = "TyivXDz4fLTaoWkhUZtMleekhBeJTzQo";
+const endpoint = create_endpoint("google", API_KEY);
+var split_endpoint = endpoint.split("/");
+var hostname = split_endpoint[2];
+var path = split_endpoint[3] + "&q=" + Search.value;
 
 //gets the search value for any google search when extension is enabled
 var search_value = document.getElementsByClassName("gLFyf").q.value 
@@ -7,6 +47,28 @@ var Search = {
     value : search_value
 };
 
-//currently throws an alert with the search value
-window.alert(search_value);
-module.exports = {Search};
+console.log(endpoint);
+console.log(hostname);
+console.log(path);
+
+const options = {
+    "method": "GET",
+    "hostname":  hostname,
+    "port": null,
+    "path": path,
+    "headers": {}
+  };
+
+//https://docs.webscrapingapi.com/google-search-api/google-search-engines/google-search-api
+// const req = http.request(options, function (res) {
+//     const chunks = [];
+  
+//     res.on("data", function (chunk) {
+//       chunks.push(chunk);
+//     });
+  
+//     res.on("end", function () {
+//       const body = Buffer.concat(chunks);
+//       console.log(body.toString());
+//     });
+//   });
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,41 +1,4 @@
-var http = require("https");
-var {Search} = require('./content.js');
-var {EndpointException}= require('./exceptions.js');
 
-
-function isAvailableEngine(engine) {
-    // generated from https://docs.webscrapingapi.com/google-search-api/google-search-engines
-    const available_engines = [
-        "google",
-        "google_maps",
-        "google_maps_reviews",
-        "google_autocomplete",
-        "google_scholar",
-        "google_scholar_profiles",
-        "google_scholar_author",
-        "google_scholar_cite",
-        "google_product",
-        "google_reverse_image",
-        "google_jobs",
-        "google_jobs_listing",
-        "google_events",
-        "google_play",
-        "google_trends"
-    ];
-
-    return available_engines.includes(engine);
-};
-
-function create_endpoint(engine, API_KEY) {
-    var endpoint = "https://serpapi.webscrapingapi.com/v1";
-    if (isAvailableEngine(engine)) {
-        endpoint += "?api_key=" + API_KEY + "&engine=" + engine;
-        return endpoint;
-    } else {
-        throw EndpointException("Invalid endpoint. Please contact admin");
-    }
-
-};
 
 function isUseful() {
     window.alert("Useful Ad!");
@@ -44,30 +7,3 @@ function isUseful() {
 function isUseless() {
     window.alert("Useless Ad!");
 }
-
-const endpoint = create_endpoint("google", process.env.API_KEY);
-var split_endpoint = endpoint.split("/");
-var hostname = split_endpoint[2];
-var path = split_endpoint[3] + "&q=" + Search.value;
-
-const options = {
-    "method": "GET",
-    "hostname": hostname,
-    "port": null,
-    "path": path,
-    "headers": {}
-  };
-
-//https://docs.webscrapingapi.com/google-search-api/google-search-engines/google-search-api
-const req = http.request(options, function (res) {
-    const chunks = [];
-  
-    res.on("data", function (chunk) {
-      chunks.push(chunk);
-    });
-  
-    res.on("end", function () {
-      const body = Buffer.concat(chunks);
-      console.log(body.toString());
-    });
-  });
